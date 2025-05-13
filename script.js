1c5fa68a8cf5da434109a685d6b47a01ecd53394
Anastasiia Lukianets <lukianets.anastasiia@gmail.com>
2025-05-13 22:30:30 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Функция для обработки отправки формы
+document
+  .getElementById("passwordForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Отменяем стандартное поведение формы
+
+    const enteredPassword = document.getElementById("password").value;
+    const correctPassword = "12345"; // Заданный правильный пароль
+
+    // // Сравниваем введённый пароль с правильным
+    // if (enteredPassword === correctPassword) {
+    //   console.log("Пароль верный!");
+    // } else {
+    //   console.log("Неверный пароль.");
+    // }
+
+    const murchikLovesMurka = false;
+    const murkaLubitMurchika = true;
+    const isLove = murchikLovesMurka && murkaLubitMurchika;
+    //const murkaHasMurchik = true;
+    console.log(isLove);
+
+    const testObj = {
+      name: "Murchik",
+      age: 38,
+    };
+
+    console.log(testObj);
+  });
